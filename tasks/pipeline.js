eb0e0336e47356fe12d0bd0b1b5e010334d9db13
arturusfury <bri.reynolds@gmail.com>
2016-11-08 19:34:06 -0600

    modified pipeline to allow jquery loading

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -27,6 +27,8 @@ var jsFilesToInject = [
 
   // Load sails.io before everything else
   'js/dependencies/sails.io.js',
+  'js/dependencies/jquery.js',
+  'js/dependencies/angular.js',
 
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
