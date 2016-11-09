ccb2070b73c7852edee9a146d0def8474c54e78e
arturusfury <bri.reynolds@gmail.com>
2016-11-08 21:00:40 -0600

    added signup post route for server side

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,5 +6,47 @@
  */
 
 module.exports = {
+  signup: function(req, res) {
+    console.log('Backend Signup');
 
+    var Passwords = require('machinepack-passwords');
+
+    Passwords.encryptPassword({
+      password: req.param('password'),
+      difficulty: 10,
+    }).exec({
+      error: function(err) {
+        return res.negotiate(err);
+      },
+      success: function(encryptedPassword) {
+        require('machinepack-gravatar').getImageUrl({
+          emailAddress: req.param('email')
+        }).exec({
+          error: function(err) {
+            return res.negotiate(err);
+          },
+          success: function(gravatarUrl) {
+            User.create({
+              name: req.param('name'),
+              email: req.param('email'),
+              password: encryptedPassword,
+              lastLoggedIn: new Date(),
+              gravatarUrl: gravatarUrl
+            }, function userCreated(err, newUser) {
+              if (err) {
+                console.log('Error: ' + err);
+                return res.negotiate(err);
+              }
+
+              // Session Variable
+
+              return res.json({
+                id: newUser.id
+              });
+            })
+          }
+        })
+      }
+    })
+  }
 };
--- api/models/User.js
+++ api/models/User.js
@@ -8,7 +8,27 @@
 module.exports = {
 
   attributes: {
-
+    name: {
+      type: 'String',
+      required: true
+    },
+    email: {
+      type: 'String',
+      email: true,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: 'String',
+      required: true
+    },
+    lastLoggedIn: {
+      type: 'Date',
+      required: true,
+      defaultsTo: new Date(0)
+    },
+    gravatarUrl: {
+      type: 'String'
+    }
   }
 };
-
