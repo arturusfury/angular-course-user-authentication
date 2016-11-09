1464b15360d958cce46d3126e245270976999f6f
arturusfury <bri.reynolds@gmail.com>
2016-11-08 18:25:05 -0600

    added signup view and injected dependencies

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -31,6 +31,9 @@ var jsFilesToInject = [
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
 
+  'js/public/signup/SignupMod.js',
+  'js/public/signup/SignupCtrl.js',
+
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
   'js/**/*.js'
@@ -83,5 +86,3 @@ module.exports.templateFilesToInject = templateFilesToInject.map(function(tplPat
   }
   return require('path').join('assets/',tplPath);
 });
-
-
